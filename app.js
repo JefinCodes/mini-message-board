174ae54d596cf40a5f613293e56026cdb0add8bd
jefin <techy.jefinjoji@gmail.com>
2025-10-15 22:57:23 +0530

    feat: add 404 page for undefined routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("node:path");
 const indexRouter = require("./routes/indexRouter");
+const { display404Page } = require("./controllers/routeNotFoundController");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -9,6 +10,7 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", indexRouter);
+app.get("{*splat}", display404Page);
 
 const PORT = 3000;
 app.listen(PORT, (error) => {
---
+++ controllers/routeNotFoundController.js
@@ -0,0 +1,5 @@
+function display404Page(req, res) {
+    res.render("404");
+};
+
+module.exports = { display404Page };
