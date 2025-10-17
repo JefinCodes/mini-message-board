fb3c43884a9892b07a2eb062191be3da9817834a
jefin <techy.jefinjoji@gmail.com>
2025-10-18 02:57:09 +0530

    feat: add custom message not found error handler

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,6 +1,10 @@
 const { messages } = require('../messageStore');
+const MessageNotFoundError = require("../errors/MessageNotFoundError");
 
 function getMessage(req, res) {
+    if(req.params.index >= messages.length){
+        throw new MessageNotFoundError("Message not found");
+    }
     res.render("message", { message: messages[req.params.index] });
 };
 
